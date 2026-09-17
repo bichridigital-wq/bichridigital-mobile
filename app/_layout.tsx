31a3469b4afffd05e978a9b367e3a9399206b44d
BOUNAMA NIANG <bichridigital@gmail.com>
2026-09-17 14:37:46 +0000

    fix: register auth routes in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,10 @@ export default function RootLayout() {
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="video/[videoId]" options={{ headerShown: false }} />
               <Stack.Screen name="emission/[slug]" options={{ headerShown: false }} />
-              <Stack.Screen name="auth" options={{ headerShown: false }} />
+              <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+<Stack.Screen name="auth/register" options={{ headerShown: false }} />
+<Stack.Screen name="auth/forgot-password" options={{ headerShown: false }} />
+<Stack.Screen name="auth/reset-password" options={{ headerShown: false }} />
             </Stack>
             <StatusBar style="light" />
           </ThemeProvider>
